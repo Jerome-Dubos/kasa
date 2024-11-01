e133de80aba80855cb138ca4d5c099b9c311fc8a
Jérôme DUBOS <jeromedubos67@gmail.com>
2024-11-01 17:13:59 +0100

    Reste responsive logements

--- src/pages/logement/Logement.jsx
+++ src/pages/logement/Logement.jsx
@@ -1,49 +1,48 @@
-import React, {  useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import './Logement.scss'
-import Collapse from '../../components/Collapse/Collapse'
-import Logements from '../../datas/Logements.json'
-import Carousel from '../../components/Carrousel/Carrousel'
-import Rating from '../../components/Rating/Rating'
-
+import React from 'react'; // Assurez-vous d'importer React
+import { useParams, Navigate } from 'react-router-dom';
+import './Logement.scss';
+import Collapse from '../../components/Collapse/Collapse';
+import Logements from '../../datas/Logements.json';
+import Carousel from '../../components/Carrousel/Carrousel';
+import Rating from '../../components/Rating/Rating';
 
 const Logement = () => {
   const { id } = useParams();
-  const logement = Logements.find(l => l.id === id);
+  const logement = Logements.find((l) => l.id === id);
 
   if (!logement) {
-    return <p>Logement non trouvé</p>;
+    return <Navigate to='/Error' />;
   }
 
   return (
     <main>
-    <div className='carousel'>
-      <Carousel pictures={logement.pictures} />
-    </div>
-    <section className='description'>
-      <div>
-        <h2 className='title'>{logement.title}</h2>
-        <p className='location'>{logement.location}</p>
+      <div className='carousel'>
+        <Carousel pictures={logement.pictures} />
       </div>
-      <div className='host'>
-        <p>{logement.host.name}</p>
-        <img src={logement.host.picture} alt="Hôte" />
+      <section className='description'>
+        <div>
+          <h2 className='title'>{logement.title}</h2>
+          <p className='location'>{logement.location}</p>
+        </div>
+        <div className='host'>
+          <p>{logement.host.name}</p>
+          <img src={logement.host.picture} alt="Hôte" />
         </div>
       </section>
-      <section className='description'>
+      <section className='tags-rating'>
         <div className='tags'>
           {logement.tags.map((tag, index) => (
-        <div className='tag' key={index}>{tag}</div>
-        ))}
+            <div className='tag' key={index}>{tag}</div>
+          ))}
         </div>
         <Rating rating={logement.rating} />
-       </section>
-       <div className='collapsesLog'>
+      </section>
+      <div className='collapsesLog'>
         <Collapse title="Description" content={logement.description} />
-        <Collapse className='equipment' title="Équipements" content={logement.equipments} />
+        <Collapse title="Équipements" content={logement.equipments} />
       </div>
     </main>
   );
 };
 
-export default Logement
+export default Logement;
